64fc43a36788fb876740407f5dc1bfecbd95a13c
masl3noki <90714327+masl3noki@users.noreply.github.com>
2023-11-16 22:00:27 +0300

    ДЗ №2: Выполненное первое задание
    - Построил mae/mse/регуляризации;
    - Заполнил .ipynb;
    - Закомментировал каждый шаг реализации.

--- HW02/Loss and derivatives/derivatives.py
+++ HW02/Loss and derivatives/derivatives.py
@@ -12,11 +12,11 @@ class LossAndDerivatives:
         Return : float
             single number with MSE value of linear model (X.dot(w)) with no bias term
             on the selected dataset.
-        
+
         Comment: If Y is two-dimentional, average the error over both dimentions.
         """
 
-        return np.mean((X.dot(w) - Y)**2)
+        return np.mean((X.dot(w) - Y) ** 2)
 
     @staticmethod
     def mae(X, Y, w):
@@ -24,7 +24,7 @@ class LossAndDerivatives:
         X : numpy array of shape (`n_observations`, `n_features`)
         Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
         w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
-                
+
         Return: float
             single number with MAE value of linear model (X.dot(w)) with no bias term
             on the selected dataset.
@@ -32,8 +32,7 @@ class LossAndDerivatives:
         Comment: If Y is two-dimentional, average the error over both dimentions.
         """
 
-        # YOUR CODE HERE    
-        return 
+        return np.mean(abs(X.dot(w) - Y))
 
     @staticmethod
     def l2_reg(w):
@@ -45,9 +44,8 @@ class LossAndDerivatives:
 
         Computes the L2 regularization term for the weight matrix w.
         """
-        
-        # YOUR CODE HERE
-        return 
+
+        return np.sum(w**2)
 
     @staticmethod
     def l1_reg(w):
@@ -56,38 +54,47 @@ class LossAndDerivatives:
 
         Return : float
             single number with sum of the absolute values of the weight matrix ( \sum_{ij} |w_{ij}| )
-        
+
         Computes the L1 regularization term for the weight matrix w.
         """
 
-        # YOUR CODE HERE
-        return 
+        return np.sum(abs(w))
 
     @staticmethod
     def no_reg(w):
         """
         Simply ignores the regularization
         """
-        return 0.
-    
+        return 0.0
+
     @staticmethod
     def mse_derivative(X, Y, w):
         """
         X : numpy array of shape (`n_observations`, `n_features`)
         Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
         w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
-        
+
         Return : numpy array of same shape as `w`
 
         Computes the MSE derivative for linear regression (X.dot(w)) with no bias term
         w.r.t. w weight matrix.
-        
+
         Please mention, that in case `target_dimentionality` > 1 the error is averaged along this
         dimension as well, so you need to consider that fact in derivative implementation.
         """
 
-        # YOUR CODE HERE
-        return 
+        # Что просят: 
+        # 1) вычислить производную ||Xw - Y||^2 по норме 2 
+        # 2) усреднить по n_observations и target_dimentionality
+
+        derivative = 2 * (X.T) @ (X@w - Y)
+
+        # А по факту Y.size = n_observations * target_dimentionality
+        # n_observations = X.shape[0]
+        # target_dimentionality = Y.shape[1]
+
+        # return (derivative / (n_observations * target_dimentionality))
+        return (derivative / (Y.size))
 
     @staticmethod
     def mae_derivative(X, Y, w):
@@ -95,18 +102,28 @@ class LossAndDerivatives:
         X : numpy array of shape (`n_observations`, `n_features`)
         Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
         w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
-        
+
         Return : numpy array of same shape as `w`
 
         Computes the MAE derivative for linear regression (X.dot(w)) with no bias term
         w.r.t. w weight matrix.
-        
+
         Please mention, that in case `target_dimentionality` > 1 the error is averaged along this
         dimension as well, so you need to consider that fact in derivative implementation.
         """
 
-        # YOUR CODE HERE
-        return 
+        # Что просят: 
+        # 1) вычислить производную ||Xw - Y||^2 по норме 2 
+        # 2) усреднить по n_observations и target_dimentionality
+
+        # Очень хочется сделать sign(X@w - Y) @ X, однако разные размерности => домножаем в начале на X.T
+        derivative = X.T @ np.sign((X@w - Y))
+
+        # А по факту Y.size = n_observations * target_dimentionality
+        # n_observations = X.shape[0]
+        # target_dimentionality = Y.shape[1]
+
+        return (derivative / (Y.size))
 
     @staticmethod
     def l2_reg_derivative(w):
@@ -117,9 +134,9 @@ class LossAndDerivatives:
 
         Computes the L2 regularization term derivative w.r.t. the weight matrix w.
         """
+        # Собственно, это обычный dx^2/dx = 2x
 
-        # YOUR CODE HERE
-        return 
+        return 2*w
 
     @staticmethod
     def l1_reg_derivative(w):
@@ -131,9 +148,9 @@ class LossAndDerivatives:
 
         Computes the L1 regularization term derivative w.r.t. the weight matrix w.
         """
-
-        # YOUR CODE HERE
-        return 
+        # Просто нужен знак        
+        
+        return np.sign(w)
 
     @staticmethod
     def no_reg_derivative(w):
@@ -141,4 +158,3 @@ class LossAndDerivatives:
         Simply ignores the derivative
         """
         return np.zeros_like(w)
-
